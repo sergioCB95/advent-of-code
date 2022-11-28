bf77a8de6981ecdd2ae0ad8dd2aef55265772570
Sergio Cámara Braña <sergio.camara@guidesmiths.com>
2022-11-28 09:24:21 +0100

    21-2 not finished

--- 2021/21/21-2.js
+++ 2021/21/21-2.js
@@ -1,3 +1,5 @@
+const WIN_POINTS = 21;
+
 const initTimes = () => {
     const times = Array(10).fill(0);
     for (let i = 1; i < 4; i++) {
@@ -11,7 +13,9 @@ const initTimes = () => {
     return times;
 }
 
-const movePlayer = (_positions, _times) => {
+const movePlayer = (_positions, _times, _winners) => {
+    const newWinners = [..._winners];
+    newWinners.push(0);
     const newPositions = Array(9).fill().map(() => ({
         count: 0,
         scores: Array(22).fill(0),
@@ -22,30 +26,67 @@ const movePlayer = (_positions, _times) => {
             newPositions[nextPos].count += (x * _positions[i].count);
 
             _positions[i].scores.forEach((score, scoreIndex) => {
+                if (scoreIndex >= WIN_POINTS) {
+                    return;
+                }
                 let nextScore = scoreIndex + nextPos + 1;
-                nextScore = nextScore > 21 ? 21 : nextScore;
-                newPositions[nextPos].scores[nextScore] += x * score;
+                nextScore = nextScore > WIN_POINTS ? WIN_POINTS : nextScore;
+                const scoreMove = x * score;
+                newPositions[nextPos].scores[nextScore] += scoreMove;
+
+                if (nextScore === WIN_POINTS) {
+                    newWinners[newWinners.length - 1] += scoreMove;
+                }
             });
         });
     }
-    return newPositions;
+    return [newPositions, newWinners];
+}
+
+const IsPlaying = (_positions) => !!_positions
+    .flatMap(position => position.scores.filter((x, i) => i !== WIN_POINTS && x)).length
+
+const calcPlayer = (startPosition, _times) => {
+    let winners = [];
+    let positions = Array(9).fill().map(() => ({
+        count: 0,
+        scores: Array(22).fill(0),
+    }));
+    positions[startPosition].count = 1;
+    positions[startPosition].scores[0] = 1;
+
+    while (IsPlaying(positions)) {
+        [positions, winners] = movePlayer(positions, _times, winners);
+    }
+
+    const sum = positions.reduce((acc, curr) => acc + curr.count , 0);
+    console.log(sum);
+    console.log(winners);
+    return winners;
 }
 
 
 const times = initTimes();
-let positions = Array(9).fill().map(() => ({
-    count: 0,
-    scores: Array(22).fill(0),
-}));
-positions[3].count = 1;
-positions[3].scores[0] = 1;
+const possibilities = times.reduce((acc, curr) => acc + curr, 0);
+const winner1 = calcPlayer(3, times);
+const winner2 = calcPlayer(7, times);
 
-for (let i = 0; i < 2; i++) {
-    positions = movePlayer(positions, times);
+const length = winner1.length > winner2.length ? winner1.length : winner2.length;
+
+let wins1 = 0;
+let wins2 = 0;
+let permutations1 = possibilities;
+let permutations2 = possibilities;
+for (let i = 0; i < length; i++) {
+    permutations1 = permutations1 - winner1[i];
+    wins1 += winner1[i] * permutations2;
+    wins2 += winner2[i] ? winner2[i] * permutations1 : 0;
+    permutations1 = (permutations1 * possibilities);
+    permutations2 = (permutations2 * possibilities) - winner2[i];
 }
 
-console.log(positions);
-
+console.log(wins1);
+console.log(wins2);
 
 
 
