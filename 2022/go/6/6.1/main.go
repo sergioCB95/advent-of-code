f09e4131705eba34cf7849199be1e51b784dd4a2
Sergio Cámara Braña <sergio.camara@guidesmiths.com>
2022-12-06 10:09:09 +0100

    day 6

---
+++ 2022/go/6/6.1/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"io/ioutil"
+	"strings"
+)
+
+func getFileAsString(fileName string) string {
+	data, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+	return string(data)
+}
+
+func main() {
+	var result int
+	input := getFileAsString("../data.txt")
+	initI := 0
+	endI := 4
+	for endI < len(input) {
+		found := true
+		marker := input[initI:endI]
+		println(marker)
+		for _, r := range marker {
+			count := strings.Count(marker, string(r))
+			println(count)
+			if count > 1 {
+				found = false
+			}
+		}
+		if found {
+			result = endI
+			break
+		}
+		println()
+		initI += 1
+		endI += 1
+	}
+	println(result)
+}
